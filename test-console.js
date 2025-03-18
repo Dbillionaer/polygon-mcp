c366940c9321d5fd18173d14610e3d0f2634f557
dbillionaer <dbillionaer@gmail.com>
2025-03-18 09:52:54 -0400

    refactor: reorganize files into polygonmcp directory

--- test-console.js
+++
@@ -1,4 +0,0 @@
-// test-console.js - Simple test script to verify console output
-
-console.log('This is a test of the console output');
-console.log('If you can see this message, the console output is working');
